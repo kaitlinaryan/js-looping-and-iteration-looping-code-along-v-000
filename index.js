8f8eabec19ad3bd2e99d8e3404a740fec1f8a6e2
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-23 16:50:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function printBadges(name) {
- for (let i = 0; i < name.length; i++) {
-   console.log('Welcome ${name}! You are employee #${i - 1}.')
+function printBadges(names) {
+ for (let i = 0; i < names.length; i++) {
+   console.log('Welcome ${names}! You are employee #${i - 1}.')
  }
  return name;
 }
