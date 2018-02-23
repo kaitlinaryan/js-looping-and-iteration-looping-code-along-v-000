3e1b75d0f992d2e2d6321842ff155c0c8f8a0d4b
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-23 17:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
  return names;
 }
 
-function tailsNeverFails {
-let tails = 0;
 
-while (Math.random() >= 0.5) {
-  tails++;
-}
-return 'You got ${tails} tails in a row!';
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
 }
